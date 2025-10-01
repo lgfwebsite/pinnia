a7607f16aced8999b117bda9607547f7d297d833
lgfwebsite <tulip_1113@qq.com>
2025-10-01 20:18:37 +0800

    结构拆分，数据渲染分类

--- vue-project/src/views/home/index.vue
+++ vue-project/src/views/home/index.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>
+  <!-- <div>
     我是home页面
+  </div> -->
+  <div class="container">
+    <Homecategory></Homecategory>
+    <HomeBanner></HomeBanner>
   </div>
+  <Homenew></Homenew>
+  <HomeProduct></HomeProduct>
+  <HomeProduct></HomeProduct>
+
 </template>
+<script setup lang="ts">
+import HomeBanner from './compontents/homebanner.vue'
+import HomeCategory from './compontents/homecategory.vue'
+import HomeNew from './compontents/homenew.vue'
+import HomeHot from './compontents/homehot.vue'
+import HomeProduct from './compontents/homeproduct.vue'
+import Homecategory from './compontents/homecategory.vue'
+import Homenew from './compontents/homenew.vue'
+</script>
