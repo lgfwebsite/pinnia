a0b9568b77584f2f936483a9f434bfc4a076156e
lgfwebsite <tulip_1113@qq.com>
2025-10-10 00:37:27 +0800

    列表无限加载

--- vue-project/src/views/subcategory/index.vue
+++ vue-project/src/views/subcategory/index.vue
@@ -16,20 +16,40 @@ onMounted(()=>{
 })
 //基础列表渲染
 const goodlist=ref([])
-const resData=ref({
-  categoryId:route.params.id,
-  page:1,
-  pageSize:20,
-  sortField: 'publishTime'
-})
+const resData=ref({categoryId: route.params.id,
+    page: 1,
+    pageSize: 20,
+    sortField: 'publishTime'})
 const getGoodlist=async ()=>{
-  const res=await getSubCategoryApi(resData)
+  const res=await getSubCategoryApi(resData.value)
   goodlist.value =res.result.items
   console.log(res)
 }
 onMounted(()=>{
   getGoodlist()
 })
+//tab切换回调
+//切换更多
+const disabled=ref(false)
+const tabChange=()=>{
+  console.log("tab切换了",resData.value.sortField)
+  resData.value.page=1
+  getGoodlist()
+}
+//列表更多加载
+const load= async()=>{
+  console.log('加载更多数据了')
+
+  //获取下一页数据
+  resData.value.page++
+  const res= await getSubCategoryApi(resData.value)
+  goodlist.value=[...goodlist.value,...res.result.items]
+
+  //加载完毕 停止监听
+  if(res.result.items.length===0){
+    disabled.value = true
+  }
+}
 </script>
 
 <template>
@@ -43,12 +63,12 @@ onMounted(()=>{
             </el-breadcrumb>
         </div>
         <div class="sub-container">
-            <el-tabs>
+            <el-tabs v-model="resData.sortField" @tab-change="tabChange">
                 <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
                 <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
                 <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
             </el-tabs>
-            <div class="body">
+            <div class="body" v-infinite-scroll="load" :infinite-scroll-disabled="disabled">
                 <!-- 商品列表-->
                <gooditem v-for="good in goodlist" :key="good.id" :good="good"></gooditem>
             </div>
