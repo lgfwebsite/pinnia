b2edf44216fadf103450591ec8908ea3bf9d7fac
lgfwebsite <tulip_1113@qq.com>
2025-10-12 15:20:45 +0800

    滑块跟随移动

--- vue-project/src/components/ImageView/index.vue
+++ vue-project/src/components/ImageView/index.vue
@@ -1,5 +1,7 @@
 <script setup>
-import { ref } from "vue";
+import { ref, watch } from "vue";
+import { useMouseInElement } from "@vueuse/core";
+
 // 图片列表
 const imageList = [
     "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
@@ -13,19 +15,51 @@ const enterhander=(i)=>{
     active.value=i
 }
 
+// 获取鼠标位置
+const target=ref(null)
+const {elementX, elementY, isOutside } = useMouseInElement(target)
+const left=ref(0)
+const top=ref(0)
+//监听x，y的变化
+watch([elementX,elementY],()=>{
+    console.log('x,y',elementX.value,elementY.value,isOutside.value)
+    //横向距离
+    if(elementX.value>100 && elementX.value<300){
+        console.log('在中间')
+        top.value=elementX-100
+    }
+    //纵向距离
+    if(elementY.value>100 && elementY.value<300){
+        console.log('在中间')
+        left.value=elementY-100
+}
+//处理边界
+    if(elementX.value<100){
+        top.value=0
+    }
+    if(elementX.value>300){
+        top.value=200
+    }
+    if(elementY.value<100){
+        left.value=0
+    }
+    if(elementY.value>300){
+      left.value=200
+    }
 
+
+})
 </script>
 
-
 <template>
-    <!-- {{ elementX }}, {{ elementY }}, {{ isOutside }} -->
+    {{ elementX}}, {{ elementY}}, {{isOutside}}
     <div class="goods-image">
 
         <!-- 左侧大图-->
         <div class="middle" ref="target">
             <img :src="imageList[active]" alt="" />
             <!-- 蒙层小滑块 -->
-            <div class="layer" ></div>
+            <div class="layer" :style="{left:`${left}px`,top:`${top}px`}"></div>
         </div>
         <!-- 小图列表 -->
         <ul class="small">
